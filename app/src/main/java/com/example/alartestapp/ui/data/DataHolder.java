ddffd53e498daa5a2e7453f055367d0f49dbb8d2
savelgmx <savel@gmx.com>
2021-07-14 16:13:29 +0700

    //TODO Write bind in DataHolder and rest of that class //TODO response display in layout //TODO initial values for usernamre/pswd on first screen //TODO third screen with Google maps

--- app/src/main/java/com/example/alartestapp/ui/data/DataHolder.java
+++ app/src/main/java/com/example/alartestapp/ui/data/DataHolder.java
@@ -2,17 +2,59 @@ package com.example.alartestapp.ui.data;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
 
 
+import com.example.alartestapp.R;
 import com.example.alartestapp.model.Data;
+import com.example.alartestapp.model.Datum;
 
 public class DataHolder extends RecyclerView.ViewHolder {
+
+    private TextView mName;
+    private TextView mCountry;
+    private TextView mLat;
+    private TextView mLon;
+
     public DataHolder(View itemView) {
         super(itemView);
+        mName = itemView.findViewById(R.id.tv_name);
+        mCountry=itemView.findViewById(R.id.tv_country);
+        mLat=itemView.findViewById(R.id.tv_lat);
+        mLon=itemView.findViewById(R.id.tv_lon);
+
 
     }
 
-    public void bind(Data data, DataAdapter.OnItemClickListener mOnItemClickListener) {
+    public void bind(Datum item, DataAdapter.OnItemClickListener onItemClickListener) {
+
+
+
+        mName.setText(item.getName());
+        mCountry.setText("westeros");
+        mLon.setText("lon");
+        mLat.setText("hh");
+
 
     }
+/*
+    public void bind(Project item, ProjectsAdapter.OnItemClickListener onItemClickListener) {
+        Picasso.with(mImage.getContext()).load(item.getCover().getPhotoUrl())
+                .fit()
+                .into(mImage);
+
+        mName.setText(item.getName());
+        mUsername.setText(item.getOwners().get(FIRST_OWNER_INDEX).getUsername());
+        mPublishedOn.setText(DateUtils.format(item.getPublishedOn()));
+
+        if (onItemClickListener != null) {
+            itemView.setOnClickListener(v -> onItemClickListener.onItemClick(
+                    item.getOwners()
+                            .get(FIRST_OWNER_INDEX)
+                            .getUsername()
+            ));
+        }
+    }
+
+ */
 }
